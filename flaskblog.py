ba379b1662a73288c6b65bfd9988d3cdbccaf028
Daniel Ortega <oh.dany.mx@gmail.com>
2020-06-06 13:43:39 -0500

    Agregar plantillas

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Dany Ortega',
+        'title': 'My first post in the blog',
+        'content': 'My first post content in the news',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Joe Ortega',
+        'title': 'My second post in the blog',
+        'content': 'My second post content in the news',
+        'date_posted': 'April 21, 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Oh Dany</h1>"
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
